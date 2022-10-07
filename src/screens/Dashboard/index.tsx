02f393330aa4341221fd7de8ae4557193f653d42
FRANCISCO WILLIAM DE OLIVEIRA LIMA <francisco.wolima@senacsp.edu.br>
2022-10-06 22:10:54 -0300

    06-10

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { Container,Title } from './styles'
+
+export function Dashboard() {
+ return(
+      <Container>
+        <Title> Dashboard... </Title>
+      </Container>
+    )
+  
+}
+   
+  
